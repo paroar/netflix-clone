01ff2466ed63a1b7b835f757e3d2f3d2080fe23a
paroar <prodriguezarmida@gmail.com>
2021-04-22 22:37:43 +0200

    card details

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -58,6 +58,38 @@ Card.Meta = ({ children, ...restProps }) => (
   <Meta {...restProps}>{children}</Meta>
 );
 
+Card.Feature = ({ children, category, ...restProps }) => {
+  const { showFeature, itemFeature, setShowFeature } = useContext(
+    FeatureContext
+  );
+
+  return showFeature ? (
+    <Feature
+      {...restProps}
+      src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}
+    >
+      <Content>
+        <FeatureTitle>{itemFeature.title}</FeatureTitle>
+        <FeatureText>{itemFeature.description}</FeatureText>
+        <FeatureClose onClick={(showFeature) => setShowFeature(!showFeature)}>
+          <img src="/images/icons/close.png" alt="close" />
+        </FeatureClose>
+
+        <Group margin="30px 0" flexDirection="row" alignItems="center">
+          <Maturity rating={itemFeature.maturity}>
+            {itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}
+          </Maturity>
+          <FeatureText fontWeight="bold">
+            {itemFeature.genre.charAt(0).toUpperCase() +
+              itemFeature.genre.slice(1)}
+          </FeatureText>
+        </Group>
+        {children}
+      </Content>
+    </Feature>
+  ) : null;
+};
+
 Card.Item = ({ item, children, ...restProps }) => {
   const { setShowFeature, setItemFeature } = useContext(FeatureContext);
   return (
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -4,6 +4,7 @@ import SelectProfileContainer from './profile';
 import { Loading, Header, Card } from '../components';
 import { HOME } from '../constants/routes';
 import logo from '../logo.svg';
+import FooterContainer from './footer';
 
 const BrowseContainer = ({ slides }) => {
   const [category, setCategory] = useState('series');
@@ -83,9 +84,26 @@ const BrowseContainer = ({ slides }) => {
         {slideRows.map((slideItem) => (
           <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
             <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+              <p>Hello</p>
+            </Card.Feature>
           </Card>
         ))}
       </Card.Group>
+      <FooterContainer />
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
