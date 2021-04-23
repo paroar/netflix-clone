e403a46da830cc8c59e60cfe920694ee2f168fa9
paroar <prodriguezarmida@gmail.com>
2021-04-23 17:33:18 +0200

    fuse

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { FirebaseContext } from '../context/firebase';
 import SelectProfileContainer from './profile';
 import { Loading, Header, Card, Player } from '../components';
@@ -25,6 +26,18 @@ const BrowseContainer = ({ slides }) => {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre'],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
